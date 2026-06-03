26fc3fd82bee0462db918fd5b94ddb72a50968bf
Small <Small>
2026-06-03 16:02:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+下载pixeldrain网盘分享的文件
+
+分享链接：https://pixeldrain.com/u/{file_id}
+
+下载链接：https://pixeldrain.com/api/file/{file_id}?download
+
+下载完成后会将下载的文件分卷后提交到git分支（文件名）当中
---
+++ pixeldrain.py
@@ -0,0 +1,33 @@
+import os
+import requests
+
+def get_original_file_name(response, file_id):
+    if 'Content-Disposition' in response.headers:
+        return response.headers['Content-Disposition'].split('filename=')[-1].strip('"')
+    return file_id
+
+def download_and_get_size(url, download_folder):
+    print(f"\nDownloading from: {url}")
+
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        file_name = get_original_file_name(response, "other")
+        
+        if not os.path.exists(download_folder):
+            os.makedirs(download_folder, exist_ok=True)
+
+        save_path = os.path.join(download_folder, file_name)
+        with open(save_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        
+        # 获取下载后的文件大小（单位：字节）
+        file_size = os.path.getsize(save_path) 
+        print(f"文件已保存至 {save_path}，大小为: {file_size / (1024*1024):.2f} MB")
+    else:
+        print(f"下载失败，状态码: {response.status_code}")
+
+
+if __name__ == "__main__":
+    download_folder = f"./dist"
+    # download_and_get_size("https://pixeldrain.com/api/file/HUc25t6o?download", download_folder)
